f79f33c9e84f33564a37477760460684177ee04f
dkGithup2022 <104286091+dkGithup2022@users.noreply.github.com>
2023-02-02 22:20:00 +0900

    Time: 6 ms (62.69%), Space: 43.7 MB (32.66%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -2,8 +2,40 @@ import java.util.*;
 
 class Solution {
     public int[] twoSum(int[] nums, int target) {
+        HashMap<Integer,ArrayList<Integer>> map = new HashMap(); // num & cnt
+        for(int i = 0; i < nums.length;  i++){
+            int n = nums[i];
+            ArrayList list = map.getOrDefault(n, new ArrayList());
+            list.add(i);
+            map.put(n, list);
+        }
+       
+        
+        for(int k : map.keySet()){
+            if( map.containsKey(target- k)){
+                if( target == 2*k ){
+                    if( map.get(k).size() == 1){
+                        continue;
+                    }else{
+                        return new int[]{ map.get(k).get(0), map.get(k).get(1)};
+                    }
+                }
+                
+                return new int[]{map.get(k).get(0), map.get(target-k).get(0)};
+            }
+        }        
+        
+        
+        return null; 
+        
+    }
+}
+
+/*
+ public int[] twoSum(int[] nums, int target) {
      
      HashMap<Integer, List<Integer> > map = new HashMap();
+        
      for(int i =0 ; i< nums.length ; i++){
         int current = nums[i];
 
@@ -15,72 +47,8 @@ class Solution {
         List<Integer> list = map.getOrDefault(current , new ArrayList());
         list.add(i);
         map.put(current, list);
-
      }
+        
      return null; 
     }
-}
-
-/*
- public int[] twoSum(int[] nums, int target) {
-        Arrays.sort(nums);
-        // ㄴ> nlog n 
-        for(int i =0; i< nums.length ; i++){
-            //ㄴ> n
-            int tmp = target - nums[i];
-            int index = binarySearch(nums, i+1, nums.length-1);
-            if(index == -1)
-                continue;
-            else
-                
-            //ㄴ> logn
-        }
-    }
-    
-    public int binarySearch(int[] arr, int lo, int hi, int target){
-        
-        
-        while(lo>hi){
-            int mid = (lo+hi)/2;
-            if(arr[mid] == target)
-                return mid;
-            else if(arr[mid] > target){
-                hi = mid -1;
-            }else{
-                lo = mid +1;
-            }
-        }
-        return -1;
-    }
-    
-    
-    public int[] twoSum_(int[] nums, int target) {
-       
-        HashMap<Integer,Integer> map = new HashMap(); // value, index
-        for(int i = 0 ; i< nums.length; i++){
-            int tmp = target - nums[i];
-            if( map.keySet().contains(tmp))
-                return new int[]{map.get(tmp), i};
-            else{
-                map.put(nums[i], i);
-            }
-        }
-        return null;
-    }
-
-      HashMap<Integer, Integer> map = new HashMap();
-        // { value = [index1, index2]}
-
-        for(int i = 0; i< nums.length ; i++){
-            int tmp = target - nums[i];
-           if( map.keySet().contains(tmp))
-                return new int[]{map.get(tmp), i};
-            else{
-                map.put(nums[i], i);
-            }
-        }
-
-        return null;
-
-
 */
