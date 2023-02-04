f80a7871a63abc75501a01db2512929ccbbcdc9d
dkGithup2022 <104286091+dkGithup2022@users.noreply.github.com>
2023-02-04 22:43:54 +0900

    Time: 8 ms (55.17%), Space: 42.4 MB (71.52%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
@@ -16,13 +16,11 @@ class Solution {
         set.add(s.charAt(r));
         
         while(l != n){
-            if( r == n-1){
+            if( r == n-1)
                 l++;
-            }
-            else if(rightMovable(set, s, r+1)){
-                r++;
-                set.add(s.charAt(r));
-            }else{
+            else if(rightMovable(set, s, r+1))
+                set.add(s.charAt(++r));
+            else{
                 Character rm = s.charAt(l);
                 if(set.contains(rm))
                     set.remove(rm);
@@ -42,33 +40,5 @@ class Solution {
 }
 
 /*
-public int lengthOfLongestSubstring(String s) {
-        if(s.equals(""))
-            return 0;
-        int left = 0;
-        int right = 0;
-        HashSet<Character>  set = new HashSet();
-        set.add(s.charAt(0));
-        int max = 1;
-        while(right != s.length()){
-            // case 0 : 끝까지 간 경우
-            System.out.println("set: " + set);
-            if(right==s.length() -1){
-                max = Math.max(max,set.size());
-                break;
-            }
-            // case 1 : 오른 확장이 가능한 경우.
-            else if( !set.contains( s.charAt(right+1)) ){
-                right++;
-                set.add( s.charAt(right));
-                max = Math.max(max,set.size());
-            }
-            // case 2 : 오른 확장이 불가능 한 경우.  
-            else if( set.contains( s.charAt(right+1)) ){
-                set.remove( s.charAt(left));
-                left++;
-            }  
-        }
-        return max;
-    }
+
 */
